f2a4a39b12cd9b8fa68b29f6eb5e9e7a2a309d50
Sid Be <sa.bennaceur@gmail.com>
2024-03-20 13:38:07 +0100

    init

---
+++ petpals-caregivers/src/test/java/com/petpals/caregivers/ExampleResourceIT.java
@@ -0,0 +1,10 @@
+package com.petpals.caregivers;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+/**
+@QuarkusIntegrationTest
+class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode
+}
+ */
