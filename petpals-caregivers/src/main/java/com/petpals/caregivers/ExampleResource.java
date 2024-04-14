ccc705e0e4d138bcd3171289c6bac06c89a3fd28
Sid-Athos <sa.bennaceur@gmail.com>
2024-04-14 06:17:03 +0200

    PET-4 test

--- petpals-caregivers/src/main/java/com/petpals/caregivers/ExampleResource.java
+++ petpals-caregivers/src/main/java/com/petpals/caregivers/ExampleResource.java
@@ -2,6 +2,8 @@ package com.petpals.caregivers;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.HeaderParam;
+import jakarta.ws.rs.PathParam;
+
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -20,9 +22,9 @@ public class ExampleResource {
     }
 
     @GET
-    @Path("/withName")
+    @Path("/{name}")
     @Produces(MediaType.TEXT_PLAIN)
-    public String helloName(String name) {
+    public String helloName(@PathParam("name")  String name) {
         return "Hello " + name;
     }
 }
