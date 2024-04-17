5dc6db72dfdd2510c5a4592daa02437030a84009
Sid-Athos <sa.bennaceur@gmail.com>
2024-04-17 03:21:10 +0200

    PET-25 upgraded the inbound interceptor

--- petpals-caregivers/src/main/java/com/petpals/caregivers/bootstrap/interceptors/InboundInterceptor.java
+++ petpals-caregivers/src/main/java/com/petpals/caregivers/bootstrap/interceptors/InboundInterceptor.java
@@ -5,14 +5,18 @@ import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.logging.Logger;
 
 
 @Provider
 public class InboundInterceptor implements ContainerRequestFilter {
+    private final Logger LOG = Logger.getLogger(InboundInterceptor.class);
+
     @ConfigProperty(name = "api.key")
     String apiKey;
     @Override
     public void filter(ContainerRequestContext context) {
+        LOG.info(context.getRequest().toString());
         if (context.getHeaderString("API-KEY") == null || !context.getHeaderString("API-KEY").equals(apiKey)) {
             context.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
         }
