592794ffc8d8bf45f890dce33bb1086f2b3a7321
Sid-Athos <sa.bennaceur@gmail.com>
2024-04-18 19:07:50 +0200

    PET-25 removed OIDC

--- petpals-caregivers/src/main/java/com/petpals/caregivers/persistence/mappers/CaregiversMapper.java
+++ petpals-caregivers/src/main/java/com/petpals/caregivers/persistence/mappers/CaregiversMapper.java
@@ -5,11 +5,32 @@ import com.petpals.caregivers.persistence.entities.Groomers;
 import com.petpals.caregivers.persistence.entities.Trainers;
 import com.petpals.caregivers.persistence.entities.Vets;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "cdi")
+
 public interface CaregiversMapper {
+    @Mappings(
+            value =  {
+                    @Mapping(target = "caregiverId", ignore = true),
+                    @Mapping(target = "clients", ignore = true),
+            }
+    )
     Groomers toGroomer(CreateCaregiverCommand caregiver);
+    @Mappings(
+            value =  {
+                    @Mapping(target = "caregiverId", ignore = true),
+                    @Mapping(target = "clients", ignore = true),
+            }
+    )
     Vets toVet(CreateCaregiverCommand caregiver);
+    @Mappings(
+            value =  {
+                    @Mapping(target = "caregiverId", ignore = true),
+                    @Mapping(target = "clients", ignore = true),
+            }
+    )
     Trainers toTrainer(CreateCaregiverCommand caregiver);
 
 }
