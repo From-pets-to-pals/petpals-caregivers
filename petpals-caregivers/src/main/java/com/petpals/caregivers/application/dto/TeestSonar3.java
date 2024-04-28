4c7f791919027d578f32e10d8642c2e42683cdec
Sid-Athos <sa.bennaceur@gmail.com>
2024-04-28 20:00:30 +0200

    PET-56 Test sonar

---
+++ petpals-caregivers/src/main/java/com/petpals/caregivers/application/dto/TeestSonar3.java
@@ -0,0 +1,7 @@
+package com.petpals.caregivers.application.dto;
+
+public class TeestSonar3 {
+    public static String name(){
+        return "???";
+    }
+}
---
+++ petpals-caregivers/src/main/java/com/petpals/caregivers/application/dto/TestSonar2.java
@@ -0,0 +1,4 @@
+package com.petpals.caregivers.application.dto;
+
+public class TestSonar2 {
+}
