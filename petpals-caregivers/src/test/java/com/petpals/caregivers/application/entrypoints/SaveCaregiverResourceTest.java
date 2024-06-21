75fca281bc83d475db7e35fabb570b15786979c3
Sid-Athos <sa.bennaceur@gmail.com>
2024-06-22 01:29:17 +0200

    PET-94 CoCo

--- petpals-caregivers/src/test/java/com/petpals/caregivers/application/entrypoints/SaveCaregiverResourceTest.java
+++ petpals-caregivers/src/test/java/com/petpals/caregivers/application/entrypoints/SaveCaregiverResourceTest.java
@@ -62,4 +62,75 @@ class SaveCaregiverResourceTest {
                 .statusCode(204);
 
     }
+
+    @Test
+    void testAddVets() throws JsonProcessingException {
+        var uuid = UUIDFormatter.formatUUIDSequence(UUIDGenerator.generateUUID(),true,"");
+        Mockito.doNothing().when(caregiversPersistencePort).addGroomer(Mockito.any(Groomers.class));
+        var createCaregiver = new CreateCaregiver(
+                UUIDGenerator.generateUUID().toString(),
+                "Sid",
+                "Bennaceur",
+                "sa.bennaceur@test-db-test.com",
+                "0764017528",
+                "101, rue des Acquevilles",
+                "Suresnes",
+                "92150",
+                "France",
+                new Days[]{},
+                new SpeciesEnum[]{},
+                false,
+                0.5,
+                PalsFriendsTypes.VET,
+                false,
+                0.0,
+                0.0
+        );
+        ObjectMapper mapper = new ObjectMapper();
+        var json = mapper.writeValueAsString(createCaregiver);
+        given()
+                .header("API-KEY", apiKey)
+                .header("Content-Type", "application/json")
+                .body(json)
+                .when().post("/caregivers/create")
+                .then()
+                .statusCode(204);
+
+    }
+
+    @Test
+    void testAddTrainer() throws JsonProcessingException {
+        var uuid = UUIDFormatter.formatUUIDSequence(UUIDGenerator.generateUUID(),true,"");
+        Mockito.doNothing().when(caregiversPersistencePort).addGroomer(Mockito.any(Groomers.class));
+        var createCaregiver = new CreateCaregiver(
+                UUIDGenerator.generateUUID().toString(),
+                "Sid",
+                "Bennaceur",
+                "sa.bennaceur@test-db-test.com",
+                "0764017528",
+                "101, rue des Acquevilles",
+                "Suresnes",
+                "92150",
+                "France",
+                new Days[]{},
+                new SpeciesEnum[]{},
+                false,
+                0.5,
+                PalsFriendsTypes.TRAINER,
+                false,
+                0.0,
+                0.0
+        );
+        ObjectMapper mapper = new ObjectMapper();
+        var json = mapper.writeValueAsString(createCaregiver);
+        given()
+                .header("API-KEY", apiKey)
+                .header("Content-Type", "application/json")
+                .body(json)
+                .when().post("/caregivers/create")
+                .then()
+                .statusCode(204);
+
+    }
+
 }
